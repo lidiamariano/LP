230fa0d68fa3ba5ca27fe05b2f8110b5920998b0
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2023-07-14 14:57:28 -0300

    att

---
+++ script.js
@@ -0,0 +1,7 @@
+let ratedValue = 0;
+
+function rate(value) {
+  ratedValue = value;
+  // Aqui você pode enviar o valor da avaliação para o servidor ou fazer qualquer outra ação com ele
+  console.log("Avaliação: " + ratedValue);
+}
